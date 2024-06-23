951563f699d62c0f48bf3f4f6e27493810bd6a56
Tomas Hudson Maquieyra <hudsonmaquieyratomas@gmail.com>
2024-06-23 02:19:32 -0300

    Update TheWestWolves8.h
    MODIFICACION 23/06

--- CODIGO/TheWestWolves8.h
+++ CODIGO/TheWestWolves8.h
@@ -3,22 +3,6 @@
 /////////////////////////////////////////
 //
 #include "GPIO.h"
-/*Definicio macros pines*/
-#if GPIO_DEFINED
-#define M_A_E GPIO33
-#define M_A_1 GPIO25
-#define M_A_2 GPIO26
-#define M_B_1 GPIO27
-#define M_B_2 GPIO14
-#define M_B_E GPIO12
-#define TCRT_F_0 GPIO2
-#define TCRT_F_1 GPIO3
-#define TCRT_F_2 GPIO4
-#define TCRT_F_3 GPIO5
-#define TCRT_F_4 GPIO6
-#define TCRT_T_1 GPIO7
-#define TCRT_T_2 GPIO8
-#endif
 //
 /*LIBRERIAS BLUETOOTH*/
 #include <BTAddress.h>
@@ -26,17 +10,23 @@
 #include <BTScan.h>
 #include <BluetoothSerial.h>
 //
-/*Objeto tipo bluetooth*/
+/*Objeto tipo BluetoothSerial*/
 BluetoothSerial SerialBT;
 //
-/*Prototipo iniciacion de pines*/
-void init();
+/*Libreria control de sensores Hc-Sr04*/
+#include <UltrasonicSensor.h>
+/*Libreria control de motores*/
+#include <THMotor.h>
 //
-/*HEADER FILE MOTORES*/
+/*HEADER COMPONENTES*/
 #include "MOTORES.h"
-//
-/*LIBRERIA CON PROGRAMA DEL SUMOBOT*/
+#include "INFRARROJOS.h"
+#include "ULTRASONICOS.h"
+/*HEADER CON LA FUNCION DE PROGRAMA DEL SUMOBOT*/
 #include "WW8SUMO.h"
 //
-/*LIBRERIA CON PROGRAMA DEL SUMOBOT*/
-#include "WW8SigueLineas.h"
+/*HEADER CON LA FUNCION DE PROGRAMA DEL SIGUE LINEAS*/
+#include "WW8SIGUELINEAS.h"
+//
+/*HEADER CON LA FUNCION DE PROGRAMA DEL DESPEJA AREA*/
+#include "WW8DESPEJAAREA.h"
