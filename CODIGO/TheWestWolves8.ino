ff0a2eb30192f1fd14a302f82a13a062ba5aa605
Tomas Hudson Maquieyra <hudsonmaquieyratomas@gmail.com>
2024-06-23 02:17:46 -0300

    Update TheWestWolves8.ino
    MODIFICACION 23/06

--- CODIGO/TheWestWolves8.ino
+++ CODIGO/TheWestWolves8.ino
@@ -1,31 +1,27 @@
 #include "TheWestWolves8.h"
+
 void setup() {
-  /*Iniciando puerto Bluetooth*/
+  /*Inicializacion puerto Bluetooth*/
   Serial.begin(115200);
   SerialBT.begin("TheWestWolf_tec8");
   Serial.println("LISTO PARA EMPAREJAR...");
-  void init();
+  /*declaracion de puertos y pines*/
+  initMotores();
+  Serial.println("Pins motores definidos...");
+  initIR(); 
+  Serial.println("Pins IR sens definidos...");
+  initUltraSonicos();
+  Serial.println("Pins ultrasonicos definidos...");
   Serial.println("PINES DEFINIDOS...");
+  /*Fondos virtuales de los sensores HC-sr04*/
+  HCSR_F_I.MaxRange(MAXRANGE);
+  HCSR_F_D.MaxRange(MAXRANGE);
+  HCSR_I.MaxRange(MAXRANGE);
+  HCSR_D.MaxRange(MAXRANGE);
+  
   Serial.println("\\FIN SETUP//");
 }
 
 void loop() {
-    lecturaBT();
-    sumo();
-}
-/*Funcion iniciacion para sumo*/
-void init() {
-  pinMode(M_A_1, OUTPUT);
-  pinMode(M_A_2, OUTPUT);
-  pinMode(M_A_E, OUTPUT);
-  pinMode(M_B_1, OUTPUT);
-  pinMode(M_B_2, OUTPUT);
-  pinMode(M_B_E, OUTPUT);
-  pinMode(TCRT_F_0, INPUT);
-  pinMode(TCRT_F_1, INPUT);
-  pinMode(TCRT_F_2, INPUT);
-  pinMode(TCRT_F_3, INPUT);
-  pinMode(TCRT_F_4, INPUT);
-  pinMode(TCRT_T_1, INPUT);
-  pinMode(TCRT_T_2, INPUT);
+  sumo();
 }
