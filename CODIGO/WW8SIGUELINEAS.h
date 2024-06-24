098a2cacc1aa5b619f1e789d2c6a5d1e1db3fb38
Tomas Hudson Maquieyra <hudsonmaquieyratomas@gmail.com>
2024-06-24 00:07:25 -0300

    Update WW8SIGUELINEAS.h
    Completado de la función principal

--- CODIGO/WW8SIGUELINEAS.h
+++ CODIGO/WW8SIGUELINEAS.h
@@ -9,6 +9,55 @@ bool ARRANQUE_FLAGG = true;
 //
 /*Funcion que posiciona el robot en la linea*/
 void arranque();
+//
+/*FUNCION PRINCIPAL CON EL FUNCIONAMIENTO DEL SIGUE LINEAS*/
+int sigueLineas() {
+  arranque();
+  /*CONDICION 1*/
+  /*3 SENSORES AFUERA 2 ADENTRO*/
+  if (regleta() == 0B00011100) {
+    adelante();
+    do {
+    } while (regleta() == 0B00011100);
+    return 0;
+  
+  /*CONDICION 2*/
+  /*TODOS LOS SENSORES AFUERA*/
+  } else if (regleta() == 0B00011111) {
+    MOTOR_D.setSpeed(VEL_MAX);
+    MOTOR_I.setSpeed(VEL_MAX);
+    MOTOR_D.run(RELEASE);
+    MOTOR_I.run(FORWARD);
+    do {
+    } while (regleta() > 0B00011100);
+    return 0;
+  /*CONDICION 4*/
+  /*TODOS LOS SENSORES ADENTRO*/
+  } else if (regleta() == 0B00000000) {
+    MOTOR_D.setSpeed(VEL_MAX);
+    MOTOR_I.setSpeed(VEL_MAX);
+    MOTOR_D.run(FORWARD);
+    MOTOR_I.run(RELEASE);
+    do {
+    } while (regleta() < 0B00011100);
+    return 0;
+  /*CONDICION 5*/
+  /*MAS DE 3 SENSORES AFUERA*/
+  } else if (regleta() > 0B00011100) {
+    girar_derecha();
+    do {
+    } while (regleta() > 0B00011100);
+    return 0;
+  /*CONDICION 6*/
+  /*MAS DE 2 SENSORES ADENTRO*/
+  } else if (regleta() < 0B00011100) {
+    girar_izquierda();
+    do {
+    } while (regleta() < 0B00011100);
+    return 0;
+  }
+}
+//
 void arranque() {
   if (ARRANQUE_FLAGG) {
     MOTOR_D.setSpeed(VEL_MAX);
@@ -19,21 +68,15 @@ void arranque() {
     delay(5);
     adelante();
     do {
-    } while (regleta() == 0B00000000);
+    } while (regleta() == 0B00011111);
     detener();
     delay(5);
     rotar_derecha();
     do {
- 
-    } while (regleta() == 0B000000011);
+
+    } while (regleta() == 0B00011100);
     detener();
     delay(5);
     ARRANQUE_FLAGG = false;
   }
-}
-
-void sigueLineas() {
-
-  arranque();
-  
 }
